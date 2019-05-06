7932f9a890e4c28b8a9cf96eae6708f9c1c05604
Hansimov <591172499@qq.com>
2019-05-06 16:36:18 +0800

    + Reorganize script structure

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -2,31 +2,75 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from random import randint
+from shutil import copyfile
 
 src = "data/"
-dst = "data_processed/"
+dst = "dataX/"
+
+def renameFiles(src,dst):
+    for filename in os.listdir(src):
+        name, ext = os.path.splitext(filename)
+        print(name)
+        if ext == ".bmp":
+            cls = "A"
+        elif ext == ".png":
+            cls = "B"
+        elif ext == ".jpg":
+            if name == "timg":
+                cls = "D"
+            else:
+                cls = "C"
+        else:
+            cls = "X"
+        copyfile(src+filename,dst+cls+filename)
 
 imgs = []
-
-for filename in os.listdir(src):
-    tmp = cv2.imread(src+filename,0)
-    # tmp = cv2.imread(src+filename)
-    # res = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
-    res = cv2.Sobel(tmp, cv2.CV_64F,1,1, ksize=3)
-    # res = cv2.Laplacian(tmp,cv2.CV_64F,ksize=5)
-    # res = cv2.Scharr(tmp,cv2.CV_64F,1,0)
-    imgs.append(res)
+def sharpImages(dst):
+    global imgs
+    for filename in os.listdir(dst):
+        tmp = cv2.imread(dst+filename,0)
+        # res = tmp
+        # tmp = cv2.imread(src+filename)
+        # res = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
+        res = cv2.Sobel(tmp, cv2.CV_64F,1,1, ksize=3)
+        # res = cv2.Laplacian(tmp,cv2.CV_64F,ksize=5)
+        # res = cv2.Scharr(tmp,cv2.CV_64F,1,0)
+        imgs.append([res,filename])
 
 means = []
 varis = []
-for img in imgs:
-    # means.append(np.mean(img))
-    # means.append(np.sum(img**2)/img.size)
-    means.append(np.sum(np.absolute(img))/img.size)
-    # varis.append(np.var(img))
-    varis.append(np.std(img))
-    plt.plot(means,varis,'ro')
-    # plt.scatter(means, varis)
+names = []
+def extractFeatures(imgs):
+    global means, varis, names
+    for img in imgs:
+        pixs = img[0]
+        # means.append(np.mean(pixs))
+        # means.append(np.sum(pixs**2)/img.size)
+        means.append(np.sum(np.absolute(pixs))/pixs.size)
+        # varis.append(np.var(pixs))
+        varis.append(np.std(pixs))
+        names.append(img[1])
+        # plt.scatter(means, varis)
+    means = np.array(means)
+    varis = np.array(varis)
+    [means, varis] = list(map(lambda x: np.interp(x, (x.min(),x.max()),(0,10)), [means, varis]))
+    plt.plot(means, varis, 'ro')
+    plt.show()
 
-print(imgs[-1],imgs[-2])
-plt.show()
+def calcDistance(a,b):
+    # a,b is n-d vector
+    
+    
+    return dist
+
+def clusterImages(k, imgs):
+    clusters = [[] for _ in range(k)]
+
+if __name__ == '__main__':
+    # renameFiles(src,dst)
+    sharpImages(dst)
+    extractFeatures(imgs)
+    # clusterImages(k, imgs)
+
+

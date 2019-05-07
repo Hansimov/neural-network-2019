85fa07b685ee983d4f5dbc381f7df987b8d90ce0
Hansimov <591172499@qq.com>
2019-05-07 11:42:06 +0800

    + Implement most of kmeans

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -44,33 +44,85 @@ names = []
 def extractFeatures(imgs):
     global means, varis, names
     for img in imgs:
-        pixs = img[0]
-        # means.append(np.mean(pixs))
-        # means.append(np.sum(pixs**2)/img.size)
-        means.append(np.sum(np.absolute(pixs))/pixs.size)
-        # varis.append(np.var(pixs))
-        varis.append(np.std(pixs))
+        pix = img[0]
+        # means.append(np.mean(pix))
+        # means.append(np.sum(pix**2)/img.size)
+        means.append(np.sum(np.absolute(pix))/pix.size)
+        # varis.append(np.var(pix))
+        varis.append(np.std(pix))
         names.append(img[1])
         # plt.scatter(means, varis)
     means = np.array(means)
     varis = np.array(varis)
     [means, varis] = list(map(lambda x: np.interp(x, (x.min(),x.max()),(0,10)), [means, varis]))
     plt.plot(means, varis, 'ro')
-    plt.show()
+    # plt.show()
 
 def calcDistance(a,b):
     # a,b is n-d vector
-    
-    
+    dist = 0
     return dist
 
 def clusterImages(k, imgs):
+    n = len(means)
+    mv = []
+    for idx in range(0,n):
+        mv.append(np.array([means[idx],varis[idx]]))
+
+    visited = []
     clusters = [[] for _ in range(k)]
+    clusters[0].append(randint(0,n-1))
+    visited.append(clusters[0][0])
+
+    # Initialize centers
+    tmpCenter = np.array(mv[0])
+    for i in range(1,k):
+        maxDist = 0
+        cenIdx = -1
+        for j in range(0,n):
+            if j in visited:
+                pass
+            else:
+                tmpDist = np.linalg.norm(mv[j]-tmpCenter)
+                if tmpDist>maxDist:
+                    maxDist = tmpDist
+                    cenIdx = j
+        clusters[i].append(cenIdx)
+        tmpCenter = (tmpCenter*i + mv[clusters[i][0]])/(i+1)
+
+    centers = []
+    for i in range(0,k):
+        centers.append(mv[clusters[i][0]])
+
+    # Clustering
+    for j in range(0,n):
+        if j in visited:
+            pass
+        else:
+            minDist = float("inf")
+            clsIdx = -1
+            for jj in range(len(centers)):
+                tmpDist = np.linalg.norm(mv[j]-centers[jj])
+                if tmpDist < minDist:
+                    minDist = tmpDist
+                    clsIdx = jj
+            centers[clsIdx] = (centers[clsIdx]*len(clusters[clsIdx]) + mv[j])/(len(clusters[clsIdx])+1)
+            clusters[clsIdx].append(j)
+    print(clusters)
+
+    color = ['red', 'black', 'blue', 'brown', 'green']
+    for i in range(len(clusters)):
+        for j in range(0,len(clusters[i])):
+            tmpX = mv[clusters[i][j]][0]
+            tmpY = mv[clusters[i][j]][1]
+            # print(i,j,mv[clusters[i][j]])
+            plt.plot(tmpX,tmpY,marker="o",color=color[i])
+    plt.show()
 
 if __name__ == '__main__':
     # renameFiles(src,dst)
     sharpImages(dst)
     extractFeatures(imgs)
-    # clusterImages(k, imgs)
+    clusterImages(4, imgs)
 
 

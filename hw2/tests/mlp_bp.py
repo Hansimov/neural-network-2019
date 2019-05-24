69c9a54abdf23c026079ccbcd0e452477b6c06ec
Hansimov <591172499@qq.com>
2019-05-24 16:40:23 +0800

    + Add tests

---
+++ hw2/tests/mlp_bp.py
@@ -0,0 +1,6 @@
+'''
+机器学习公开课笔记(5)：神经网络(Neural Network)——学习
+    http://www.cnblogs.com/python27/p/MachineLearningWeek05.html
+零基础入门深度学习(3) - 神经网络和反向传播算法
+    https://www.zybuluo.com/hanbingtao/note/476663
+'''
---
+++ hw2/tests/test_xor.py
@@ -0,0 +1,4 @@
+'''
+零基础入门深度学习(3) - 神经网络和反向传播算法
+    https://www.zybuluo.com/hanbingtao/note/476663
+'''

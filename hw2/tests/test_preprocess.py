b0c0f3fb3fc503899f84aef81237cc9396f0bc4f
Hansimov <591172499@qq.com>
2019-05-27 13:32:00 +0800

    + Add preprocess test

--- hw2/tests/test_preprocess.py
+++ hw2/tests/test_preprocess.py
@@ -1,14 +1,14 @@
 import cv2
 
-root = "./data_renamed/"
-# img = cv2.imread(root+"A1.bmp",0)
-# print(img.shape)
-# imgx = cv2.resize(img,(100,100))
-# print(imgx.shape)
-# cv2.imwrite("A1x.bmp",imgx)
+root = "../data_renamed/"
+img = cv2.imread(root+"A1.bmp",0)
+print(img.shape)
+imgx = cv2.resize(img,(20,20))
+print(imgx.shape)
+cv2.imwrite("A1x.bmp",imgx)
 
-img1 = cv2.imread(root+"Dtimg.jpg",0)
-print(img1.shape)
-img1x = cv2.resize(img1, (100,100))
-print(img1x.shape)
-cv2.imwrite("Dtimgx.jpg",img1x)
+# img1 = cv2.imread(root+"Dtimg.jpg",0)
+# print(img1.shape)
+# img1x = cv2.resize(img1, (10,10))
+# print(img1x.shape)
+# cv2.imwrite("Dtimgx.jpg",img1x)

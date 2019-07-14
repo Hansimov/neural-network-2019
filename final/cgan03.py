34367700f4e1b5ba56be8586fadfd522283cbd68
Hansimov <591172499@qq.com>
2019-07-14 15:43:44 +0800

    + Add report

--- final/cgan03.py
+++ final/cgan03.py
@@ -1,19 +1,20 @@
 # znxlwm/tensorflow-MNIST-cGAN-cDCGAN
-# https://github.com/znxlwm/tensorflow-MNIST-cGAN-cDCGAN
+#   https://github.com/znxlwm/tensorflow-MNIST-cGAN-cDCGAN
 
+# 导入必要的库
 import os, time, itertools, imageio, pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-# leaky_relu
-def lrelu(X, leak=0.2):
+# 带泄露的线性整流函数
+def leakyRELU(X, leak=0.2):
     f1 = 0.5 * (1 + leak)
     f2 = 0.5 * (1 - leak)
     return f1 * X + f2 * tf.abs(X)
 
-# G(z)
+# 生成器：G(z)
 def generator(x, y, isTrain=True, reuse=False):
     with tf.variable_scope('generator', reuse=reuse):
         w_init = tf.contrib.layers.xavier_initializer()
@@ -28,7 +29,7 @@ def generator(x, y, isTrain=True, reuse=False):
 
         return out
 
-# D(x)
+# 判别器：D(x)
 def discriminator(x, y, isTrain=True, reuse=False):
     with tf.variable_scope('discriminator', reuse=reuse):
         w_init = tf.contrib.layers.xavier_initializer()
@@ -36,16 +37,15 @@ def discriminator(x, y, isTrain=True, reuse=False):
         cat1 = tf.concat([x, y], 1)
 
         dense1 = tf.layers.dense(cat1, 128, kernel_initializer=w_init)
-        lrelu1 = lrelu(dense1, 0.2)
+        relu1 = leakyRELU(dense1, 0.2)
 
-        dense2 = tf.layers.dense(lrelu1, 1, kernel_initializer=w_init)
+        dense2 = tf.layers.dense(relu1, 1, kernel_initializer=w_init)
         out = tf.nn.sigmoid(dense2)
 
         return out, dense2
 
-# label preprocess
+# 标签预处理
 onehot = np.eye(10)
-
 temp_z_ = np.random.normal(0, 1, (10, 100))
 fixed_z_ = temp_z_
 fixed_y_ = np.zeros((10, 1))
@@ -56,6 +56,7 @@ for i in range(9):
     fixed_y_ = np.concatenate([fixed_y_, temp], 0)
 
 fixed_y_ = onehot[fixed_y_.astype(np.int32)].squeeze()
+
 def show_result(num_epoch, show = False, save = False, path = 'result.png'):
     test_images = sess.run(G_z, {z: fixed_z_, y: fixed_y_, isTrain: False})
 
@@ -106,9 +107,9 @@ def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
         plt.close()
 
 # training parameters
-batch_size = 200
-lr = 0.001
-train_epoch = 200
+BATCH_SIZE = 150
+LEARNING_RATE = 0.001
+EPOCHS = 200
 
 # load MNIST
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -116,10 +117,14 @@ train_set = (mnist.train.images - 0.5) / 0.5  # normalization; range: -1 ~ 1
 
 train_label = mnist.train.labels
 
-# variables : input
+
+# x: 输入
 x = tf.placeholder(tf.float32, shape=(None, 784))
+# z: 噪声
 y = tf.placeholder(tf.float32, shape=(None, 10))
+# y: 标签
 z = tf.placeholder(tf.float32, shape=(None, 100))
+
 isTrain = tf.placeholder(dtype=tf.bool)
 
 # networks : generator
@@ -130,10 +135,10 @@ D_real, D_real_logits = discriminator(x, y, isTrain)
 D_fake, D_fake_logits = discriminator(G_z, y, isTrain, reuse=True)
 
 # loss for each network
-D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones([batch_size, 1])))
-D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros([batch_size, 1])))
+D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones([BATCH_SIZE, 1])))
+D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros([BATCH_SIZE, 1])))
 D_loss = D_loss_real + D_loss_fake
-G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.ones([batch_size, 1])))
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.ones([BATCH_SIZE, 1])))
 
 # trainable variables for each network
 T_vars = tf.trainable_variables()
@@ -142,8 +147,8 @@ G_vars = [var for var in T_vars if var.name.startswith('generator')]
 
 # optimizer for each network
 with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-    D_optim = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(D_loss, var_list=D_vars)
-    G_optim = tf.train.AdamOptimizer(lr, beta1=0.5).minimize(G_loss, var_list=G_vars)
+    D_optim = tf.train.AdamOptimizer(LEARNING_RATE, beta1=0.5).minimize(D_loss, var_list=D_vars)
+    G_optim = tf.train.AdamOptimizer(LEARNING_RATE, beta1=0.5).minimize(G_loss, var_list=G_vars)
 
 # open session and initialize all variables
 sess = tf.InteractiveSession()
@@ -167,30 +172,30 @@ train_hist['total_ptime'] = []
 np.random.seed(int(time.time()))
 print('training start!')
 start_time = time.time()
-for epoch in range(train_epoch):
+for epoch in range(EPOCHS):
     G_losses = []
     D_losses = []
     epoch_start_time = time.time()
-    for iter in range(len(train_set) // batch_size):
+    for iter in range(len(train_set) // BATCH_SIZE):
         # update discriminator
-        x_ = train_set[iter * batch_size:(iter + 1) * batch_size]
-        y_ = train_label[iter * batch_size:(iter + 1) * batch_size]
+        x_ = train_set[iter * BATCH_SIZE:(iter + 1) * BATCH_SIZE]
+        y_ = train_label[iter * BATCH_SIZE:(iter + 1) * BATCH_SIZE]
 
-        z_ = np.random.normal(0, 1, (batch_size, 100))
+        z_ = np.random.normal(0, 1, (BATCH_SIZE, 100))
 
         loss_d_, _ = sess.run([D_loss, D_optim], {x: x_, y: y_, z: z_, isTrain: True})
         D_losses.append(loss_d_)
 
         # update generator
-        z_ = np.random.normal(0, 1, (batch_size, 100))
-        y_ = np.random.randint(0, 9, (batch_size, 1))
+        z_ = np.random.normal(0, 1, (BATCH_SIZE, 100))
+        y_ = np.random.randint(0, 10, (BATCH_SIZE, 1))
         y_ = onehot[y_.astype(np.int32)].squeeze()
         loss_g_, _ = sess.run([G_loss, G_optim], {z: z_, x: x_, y: y_, isTrain: True})
         G_losses.append(loss_g_)
 
     epoch_end_time = time.time()
     per_epoch_ptime = epoch_end_time - epoch_start_time
-    print('[%d/%d] - ptime: %.2f loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), train_epoch, per_epoch_ptime, np.mean(D_losses), np.mean(G_losses)))
+    print('[%d/%d] - ptime: %.2f loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), EPOCHS, per_epoch_ptime, np.mean(D_losses), np.mean(G_losses)))
     fixed_p = root + 'Fixed_results/' + model + str(epoch + 1) + '.png'
     show_result((epoch + 1), save=True, path=fixed_p)
     train_hist['D_losses'].append(np.mean(D_losses))
@@ -201,7 +206,7 @@ end_time = time.time()
 total_ptime = end_time - start_time
 train_hist['total_ptime'].append(total_ptime)
 
-print('Avg per epoch ptime: %.2f, total %d epochs ptime: %.2f' % (np.mean(train_hist['per_epoch_ptimes']), train_epoch, total_ptime))
+print('Avg per epoch ptime: %.2f, total %d epochs ptime: %.2f' % (np.mean(train_hist['per_epoch_ptimes']), EPOCHS, total_ptime))
 print("Training finish!... save training results")
 with open(root + model + 'train_hist.pkl', 'wb') as f:
     pickle.dump(train_hist, f)
@@ -210,7 +215,7 @@ show_train_hist(train_hist, save=True, path=root + model + 'train_hist.png')
 
 # png to gif
 images = []
-for e in range(train_epoch):
+for e in range(EPOCHS):
     img_name = root + 'Fixed_results/' + model + str(e + 1) + '.png'
     images.append(imageio.imread(img_name))
 imageio.mimsave(root + model + 'generation_animation.gif', images, fps=5)

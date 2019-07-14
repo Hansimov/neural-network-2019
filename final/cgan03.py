8b163d6da808724f2743e913dbad50285895c8f7
Hansimov <591172499@qq.com>
2019-07-14 23:30:36 +0800

    + Finish final report

--- final/cgan03.py
+++ final/cgan03.py
@@ -18,7 +18,6 @@ def leakyRELU(X, leak=0.2):
 def generator(x, y, isTrain=True, reuse=False):
     with tf.variable_scope('generator', reuse=reuse):
         w_init = tf.contrib.layers.xavier_initializer()
-
         cat1 = tf.concat([x, y], 1)
 
         dense1 = tf.layers.dense(cat1, 128, kernel_initializer=w_init)
@@ -26,7 +25,6 @@ def generator(x, y, isTrain=True, reuse=False):
 
         dense2 = tf.layers.dense(relu1, 784, kernel_initializer=w_init)
         out = tf.nn.tanh(dense2)
-
         return out
 
 # 判别器：D(x)
@@ -44,7 +42,7 @@ def discriminator(x, y, isTrain=True, reuse=False):
 
         return out, dense2
 
-# 标签预处理
+# 初始化随机噪声和条件变量（图像类别）
 onehot = np.eye(10)
 temp_z_ = np.random.normal(0, 1, (10, 100))
 fixed_z_ = temp_z_
@@ -57,7 +55,8 @@ for i in range(9):
 
 fixed_y_ = onehot[fixed_y_.astype(np.int32)].squeeze()
 
-def show_result(num_epoch, show = False, save = False, path = 'result.png'):
+# 记录中间结果和相关信息
+def recordResult(num_epoch, show = False, save = False, path = 'result.png'):
     test_images = sess.run(G_z, {z: fixed_z_, y: fixed_y_, isTrain: False})
 
     size_figure_grid = 10
@@ -82,7 +81,8 @@ def show_result(num_epoch, show = False, save = False, path = 'result.png'):
     else:
         plt.close()
 
-def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
+# 绘制损失函数曲线
+def plotLoss(hist, show = False, save = False, path = 'Train_hist.png'):
     x = range(len(hist['D_losses']))
 
     y1 = hist['D_losses']
@@ -94,7 +94,7 @@ def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
 
-    plt.legend(loc=4)
+    plt.legend(loc=1)
     plt.grid(True)
     plt.tight_layout()
 
@@ -106,55 +106,54 @@ def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
     else:
         plt.close()
 
-# training parameters
+# 设置训练参数
 BATCH_SIZE = 150
 LEARNING_RATE = 0.001
 EPOCHS = 200
 
-# load MNIST
+# 导入 MNIST 数据集
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 train_set = (mnist.train.images - 0.5) / 0.5  # normalization; range: -1 ~ 1
-
 train_label = mnist.train.labels
 
-
-# x: 输入
+# 创建输入变量 $x$、随机噪声 $z$、条件变量 $y$
+# x: 输入变量
 x = tf.placeholder(tf.float32, shape=(None, 784))
-# z: 噪声
+# y: 条件变量（图像类别）
 y = tf.placeholder(tf.float32, shape=(None, 10))
-# y: 标签
+# z: 随机噪声
 z = tf.placeholder(tf.float32, shape=(None, 100))
 
 isTrain = tf.placeholder(dtype=tf.bool)
 
-# networks : generator
+# 创建生成器
 G_z = generator(z, y, isTrain)
 
-# networks : discriminator
+# 创建判别器
 D_real, D_real_logits = discriminator(x, y, isTrain)
 D_fake, D_fake_logits = discriminator(G_z, y, isTrain, reuse=True)
 
-# loss for each network
+# 设置损失值变量
 D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones([BATCH_SIZE, 1])))
 D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros([BATCH_SIZE, 1])))
 D_loss = D_loss_real + D_loss_fake
 G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.ones([BATCH_SIZE, 1])))
 
-# trainable variables for each network
+# 设置每层网络的可训练变量
 T_vars = tf.trainable_variables()
 D_vars = [var for var in T_vars if var.name.startswith('discriminator')]
 G_vars = [var for var in T_vars if var.name.startswith('generator')]
 
-# optimizer for each network
+# 设置每层网络的的优化器
 with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
     D_optim = tf.train.AdamOptimizer(LEARNING_RATE, beta1=0.5).minimize(D_loss, var_list=D_vars)
     G_optim = tf.train.AdamOptimizer(LEARNING_RATE, beta1=0.5).minimize(G_loss, var_list=G_vars)
 
-# open session and initialize all variables
+# 开启 TensorFlow 的会话并初始化所有变量
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-# results save folder
+# 结果保存路径
 root = 'MNIST_cGAN_results/'
 model = 'MNIST_cGAN_'
 if not os.path.isdir(root):
@@ -162,15 +161,17 @@ if not os.path.isdir(root):
 if not os.path.isdir(root + 'Fixed_results'):
     os.mkdir(root + 'Fixed_results')
 
+# 中间变量
 train_hist = {}
 train_hist['D_losses'] = []
 train_hist['G_losses'] = []
 train_hist['per_epoch_ptimes'] = []
 train_hist['total_ptime'] = []
 
-# training-loop
+
+# 对数据集进行训练
 np.random.seed(int(time.time()))
-print('training start!')
+print('+ Training start!')
 start_time = time.time()
 for epoch in range(EPOCHS):
     G_losses = []
@@ -195,9 +196,9 @@ for epoch in range(EPOCHS):
 
     epoch_end_time = time.time()
     per_epoch_ptime = epoch_end_time - epoch_start_time
-    print('[%d/%d] - ptime: %.2f loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), EPOCHS, per_epoch_ptime, np.mean(D_losses), np.mean(G_losses)))
+    print('[%3d/%d] ptime: %.2f | d_loss: %.3f | g_loss: %.3f' % ((epoch + 1), EPOCHS, per_epoch_ptime, np.mean(D_losses), np.mean(G_losses)))
     fixed_p = root + 'Fixed_results/' + model + str(epoch + 1) + '.png'
-    show_result((epoch + 1), save=True, path=fixed_p)
+    recordResult((epoch + 1), save=True, path=fixed_p)
     train_hist['D_losses'].append(np.mean(D_losses))
     train_hist['G_losses'].append(np.mean(G_losses))
     train_hist['per_epoch_ptimes'].append(per_epoch_ptime)
@@ -206,12 +207,12 @@ end_time = time.time()
 total_ptime = end_time - start_time
 train_hist['total_ptime'].append(total_ptime)
 
-print('Avg per epoch ptime: %.2f, total %d epochs ptime: %.2f' % (np.mean(train_hist['per_epoch_ptimes']), EPOCHS, total_ptime))
-print("Training finish!... save training results")
+print('= Avg ptime per epoch: %.3f | total %d epochs time: %.3f' % (np.mean(train_hist['per_epoch_ptimes']), EPOCHS, total_ptime))
+print("+ Training finished! Saving training results ...")
 with open(root + model + 'train_hist.pkl', 'wb') as f:
     pickle.dump(train_hist, f)
 
-show_train_hist(train_hist, save=True, path=root + model + 'train_hist.png')
+plotLoss(train_hist, save=True, path=root + model + 'train_hist.png')
 
 # png to gif
 images = []
